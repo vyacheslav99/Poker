cf636d76b9f830bca9b9183ea34f0052d7cb8cda
balychkov <balychkov@bobday.ru>
2019-04-19 11:23:28 +0300

    Работа с БД. Пользовательская база данных

--- server/game/db.py
+++ server/game/db.py
@@ -1,1 +1,35 @@
-""" Адаптер для работы с БД """
+""" Адаптеры для работы с БД """
+
+import sqlite3
+
+
+def dict_factory(cursor, row):
+    return {col[0]: row[i] for i, col in enumerate(cursor.description)}
+
+
+class sqlite_connection(object):
+
+    def __init__(self, database, **kwargs):
+        self._db_con = sqlite3.connect(database, **kwargs)
+        self._db_con.row_factory = dict_factory
+
+    def get_connect(self):
+        return self._db_con
+
+    def cursor(self):
+        return self._db_con.cursor()
+
+    def execute(self, sql, params, commit=False):
+        cur = self._db_con.cursor()
+        cur.execute(sql, params)
+
+        if commit:
+            self._db_con.commit()
+
+        return cur
+
+    def commit(self):
+        self._db_con.commit()
+
+    def rollback(self):
+        self._db_con.rollback()
--- server/game/settings.py
+++ server/game/settings.py
@@ -4,9 +4,12 @@ import os
 
 
 DOCUMENT_ROOT = os.path.normpath(os.path.join(os.path.expanduser('~'), '.poker', 'server'))
+DATA_DIR = os.path.join(DOCUMENT_ROOT, 'data')
+FILESTORE_DIR = os.path.join(DATA_DIR, 'files')
 
+USER_DATABASE_DRIVER = 'sqlite'
 USER_DATABASE = {
     'sqlite': {
-        'file': os.path.join(DOCUMENT_ROOT, 'users.sqlite')
+        'database': os.path.join(DOCUMENT_ROOT, 'users.sqlite')
     }
 }
