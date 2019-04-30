6b9f4d8fdb4d3ca53f73b47be6a1b8582b46ece0
balychkov <balychkov@bobday.ru>
2019-05-01 01:30:43 +0300

    Разработка игрового движка

--- server/game/engine.py
+++ server/game/engine.py
@@ -36,18 +36,18 @@ class Engine(object):
         self._started = False
         self._game_record = []          # таблица с записью хода игры
         self._deals = []                # массив раздач
-        self._step = None               # № шага в круге (шаг - это действие одного игрока, всего шагов 3 или 4 (по кол-ву игроков))
-        self._curr_deal = None          # текущая раздача
+        self._step = 0                  # № шага в круге (шаг - это действие одного игрока, всего шагов 3 или 4 (по кол-ву игроков))
+        self._curr_deal = -1            # индекс текущей раздачи в массиве раздач
         self._table = []                # карты на столе
         self._curr_player = None        # игрок, чей сейчас ход
         self._trump = None              # козырная масть
         self._is_bet = True             # какое сейчас действие - делаются ставки (true) или ходы (false)
-        # self._to_next_deal = True       # флаг, что нужно перейти к следующей раздаче в процедуре next
-        # self._can_stop = False          # флаг, что следующим ходом игра закрывается
-        # self._prv_step = None           # предыдущий шаг в круге
-        # self._prv_deal = None           # предыдущая раздача
-        self._last_walk_player = None   # последний ходивший игрок
-        self._released_cards = []       # массив вышедших карт
+        self._to_next_deal = True       # флаг, что нужно перейти к следующей раздаче в процедуре next
+        # self._can_stop = False          # флаг, что следующим ходом игра заканчивается
+        # self._prv_step = -1             # предыдущий шаг в круге
+        # self._prv_deal = -1             # предыдущая раздача
+        self._take_player = None        # игрок, забравший взятку
+        self._released_cards = []       # массив вышедших карт (для ИИ)
 
     def _inc_index(self, idx, max_val, increment=1):
         idx += increment
@@ -55,23 +55,6 @@ class Engine(object):
             idx = 0
 
         return idx
-
-    def _reset_state(self):
-        self._started = False
-        self._game_record = []
-        self._deals = []
-        self._step = 0
-        self._curr_deal = None
-        self._table = []
-        self._curr_player = None
-        self._trump = None
-        self._is_bet = True
-        # self._to_next_deal = True
-        # self._can_stop = False
-        # self._prv_step = None
-        # self._prv_deal = None
-        self._last_walk_player = None
-        self._released_cards = []
 
     def _init_record(self):
         cap = {}
@@ -99,6 +82,31 @@ class Engine(object):
                     self._deals.append(Deal(player_idx, dt, 1 if dt == const.DEAL_BROW else 36))
                     player_idx = self._inc_index(player_idx, len(self.players))
 
+    def _deal_cards(self):
+        self._curr_deal += 1
+        self._to_next_deal = False
+
+        # если № раздачи превысил размер массива раздач - конец игре
+        if self._curr_deal >= len(self._deals):
+            self.stop()
+            return
+
+        self._take_player = None
+        self._curr_player = self._deals[self._curr_deal].player
+        #todo: тут я остановился
+
+    def start(self):
+        self._init_deals()
+        self._init_record()
+        self._deal_cards()
+        self._started = True
+
+    def stop(self):
+        pass
+
+    def next(self):
+        pass
+
     @property
     def players(self):
         return self._players
@@ -120,16 +128,3 @@ class Engine(object):
             raise GameException('В игру не добавлено ни одного игрока-человека!')
 
         self._players = players
-
-    def start(self):
-        self._reset_state()
-        self._init_deals()
-        self._init_record()
-        self._started = True
-        self.next()
-
-    def stop(self):
-        pass
-
-    def next(self):
-        pass
