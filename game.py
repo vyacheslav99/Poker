a0512f5ff9be0f15e236784b8c8244e1f513b67d
balychkov <balychkov@bobday.ru>
2019-10-01 15:16:39 +0300

    Начал разработку gui-приложения

---
+++ game.py
@@ -0,0 +1,20 @@
+import sys
+# import argparse
+from PyQt5.QtWidgets import QApplication
+
+from poker.gui import app
+
+
+def run():
+    # ap = argparse.ArgumentParser()
+
+    # ap.add_argument('--cheats_on', action='store_true', help='Включить режим читов')
+    # args = ap.parse_args()
+
+    qapp = QApplication(sys.argv)
+    poker = app.Application(qapp)
+    sys.exit(qapp.exec_())
+
+
+if __name__ == '__main__':
+    run()
---
+++ poker/gui/app.py
@@ -0,0 +1,32 @@
+from PyQt5.QtWidgets import QMainWindow, QDesktopWidget
+from PyQt5.QtGui import QIcon
+
+from poker.gui.forms.main import GameArea
+from poker.gui import config
+
+
+class Application(QMainWindow):
+
+    def __init__(self, app):
+        super().__init__()
+
+        self.app = app
+        self.area = GameArea(self)
+        self.setCentralWidget(self.area)
+        self.setWindowIcon(QIcon(config.MainIcon))
+        self.setWindowTitle(config.MainWindowTitle)
+
+        self.resize(1400, 960)
+        self.center()
+        self.show()
+        self.area.start()
+
+    def center(self):
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move((screen.width() - size.width()) / 2, (screen.height() - size.height()) / 2)
+
+    def closeEvent(self, event):
+        # self.area.save(config.AutosaveFile)
+        print('< Exit >')
+        super(Application, self).closeEvent(event)
