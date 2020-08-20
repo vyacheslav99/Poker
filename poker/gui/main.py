ce0d9c57292a2fa12bee4a26c712dbf454660779
balychkov <balychkov@bobday.ru>
2020-08-20 12:18:09 +0300

    Отрисовка итогов игры

--- poker/gui/main.py
+++ poker/gui/main.py
@@ -939,20 +939,23 @@ class MainWnd(QMainWindow):
         # todo: Пока что такая залепа
         congratulations = ('УРА, Товарищи!!!', 'Ай, молодца!', 'Учитесь, сынки!')
 
-        pos = (round(const.AREA_SIZE[0] / 2) - round(const.TABLE_AREA_SIZE[0] / 2) - 50,
-               round(const.AREA_SIZE[1] / 2) - round(const.TABLE_AREA_SIZE[1] / 2) - 100)
+        pos = (round(const.AREA_SIZE[0] / 2) - round(const.TABLE_AREA_SIZE[0] / 2) + 50,
+               round(const.AREA_SIZE[1] / 2) - round(const.TABLE_AREA_SIZE[1] / 2) + 30)
 
         self.set_text('-= Итоги игры =-', pos, Qt.cyan, 18, 65)
         winner = max([p for p in self.game.players], key=lambda x: x.last_money)
 
+        x, y = pos[0], pos[1] + 20
+
         for i, p in enumerate(self.game.players, 1):
             money = '{0:.2f}'.format(p.last_money)
             rub, kop = money.split('.')
-            self.set_text(f'{p.name}:    {p.total_scores} :: {rub} руб {kop} коп', (pos[0], pos[1] + i * 25),
+            self.set_text(f'{p.name}:    {p.total_scores} :: {rub} руб {kop} коп', (x, y + i * 30),
                           Qt.green if p == winner else Qt.yellow, 18, 65)
 
-        self.set_text(f'Победил {winner}', (pos[0], pos[1] + len(self.game.players) * 25 + 25), Qt.green, 18, 65)
-        self.set_text(random.choice(congratulations), (pos[0], pos[1] + len(self.game.players) * 25 + 50), Qt.magenta, 18, 65)
+        y = y + len(self.game.players) * 30 + 60
+        self.set_text(f'Победил {winner}', (x, y), Qt.green, 18, 65)
+        self.set_text(random.choice(congratulations), (x, y + 60), Qt.magenta, 18, 65)
 
     def clear_buttons(self):
         """ Убирает все кнопки с центральной области """
