70228730a70e85eb40f31af0e70eca9f0d488347
Балычков Вячеслав <balychkov@bobday.ru>
2021-01-07 00:22:23 +0300

    Сделано окно настройки игроков

---
+++ poker/gui/graphics.py
@@ -0,0 +1,160 @@
+import os
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+from gui import const
+from modules.core import const as core_const
+
+
+class QCard(QGraphicsPixmapItem):
+
+    def __init__(self, app, card, player, deck, back, removable=True, tooltip=None, replace_tooltip=False):
+        super(QCard, self).__init__()
+
+        self.app = app
+        self.card = card
+        self.player = player
+        self.deck = deck
+        self.back = back
+        self.removable = removable
+        self._tooltip = tooltip
+        self.replace_tooltip = replace_tooltip
+        self.side = None
+
+        self.setShapeMode(QGraphicsPixmapItem.BoundingRectShape)
+        # self.setFlag(QGraphicsItem.ItemIsMovable)
+        self.setFlag(QGraphicsItem.ItemSendsGeometryChanges)
+        self.setZValue(const.CARD_BASE_Z_VALUE)
+        self.set_std_shadow()
+
+        self.back = QPixmap(f'{const.CARD_BACK_DIR}/{self.back}.bmp')
+        if card.joker:
+            self.face = QPixmap(f'{const.CARD_DECK_DIR}/{self.deck}/j.bmp')
+        else:
+            self.face = QPixmap(f'{const.CARD_DECK_DIR}/{self.deck}/{const.CARDS[self.card.value]}{const.LEARS[self.card.lear]}.bmp')
+
+    def turn_face_up(self):
+        self.side = const.CARD_SIDE_FACE
+        self.setPixmap(self.face)
+        self.set_tooltip()
+
+        if self.player:
+            self.setCursor(Qt.PointingHandCursor)
+
+    def turn_back_up(self):
+        self.side = const.CARD_SIDE_BACK
+        self.setPixmap(self.back)
+        self.setCursor(Qt.ArrowCursor)
+        self.setToolTip('')
+
+    def is_face_up(self):
+        return self.side == const.CARD_SIDE_FACE
+
+    def set_tooltip(self):
+        if self.replace_tooltip and self._tooltip:
+            val = self._tooltip
+        else:
+            c = 'red' if self.card.lear > 1 else 'navy'
+            val = f'{core_const.CARD_NAMES[self.card.value]} <span style="color:{c}">{core_const.LEAR_SYMBOLS[self.card.lear]}</span>'
+            if self.card.joker:
+                val = f'Джокер ({val})'
+            if self._tooltip:
+                val = f'{self._tooltip} {val}'
+
+        self.setToolTip(val)
+
+    def set_color_shadow(self):
+        sh = QGraphicsDropShadowEffect()
+        sh.setBlurRadius(30)
+        sh.setOffset(5)
+        sh.setColor(Qt.green)
+        self.setGraphicsEffect(sh)
+
+    def set_std_shadow(self):
+        sh = QGraphicsDropShadowEffect()
+        sh.setBlurRadius(50)
+        self.setGraphicsEffect(sh)
+
+    def mousePressEvent(self, e):
+        if self.player and self.is_face_up():
+            self.app.card_click(self)
+
+        super(QCard, self).mousePressEvent(e)
+
+
+class Face(QGraphicsPixmapItem):
+
+    def __init__(self, player):
+        super(Face, self).__init__()
+        self.player = player
+        self.setShapeMode(QGraphicsPixmapItem.BoundingRectShape)
+        self.setFlag(QGraphicsItem.ItemSendsGeometryChanges)
+
+    @property
+    def player(self):
+        return self._player
+
+    @player.setter
+    def player(self, player):
+        self._player = player
+
+        if player.avatar and os.path.exists(f'{const.PROFILES_DIR}/{player.uid}/{player.avatar}'):
+            self.setPixmap(QPixmap(f'{const.PROFILES_DIR}/{player.uid}/{player.avatar}'))
+        elif os.path.exists(f'{const.FACE_DIR}/{self._player.name}.bmp'):
+            self.setPixmap(QPixmap(f'{const.FACE_DIR}/{self._player.name}.bmp'))
+        else:
+            self.setPixmap(QPixmap(f'{const.FACE_DIR}/noImage.png'))
+
+
+class Face2(QPixmap):
+
+    def __init__(self, player):
+        self._player = player
+
+        if player.avatar and os.path.exists(f'{const.PROFILES_DIR}/{player.uid}/{player.avatar}'):
+            self._image_path = f'{const.PROFILES_DIR}/{player.uid}/{player.avatar}'
+        elif os.path.exists(f'{const.FACE_DIR}/{self._player.name}.bmp'):
+            self._image_path = f'{const.FACE_DIR}/{self._player.name}.bmp'
+        else:
+            self._image_path = f'{const.FACE_DIR}/noImage.png'
+
+        super(Face2, self).__init__(self._image_path)
+
+    @property
+    def player(self):
+        return self._player
+
+    @property
+    def image_path(self):
+        return self._image_path
+
+
+class Lear(QGraphicsPixmapItem):
+
+    def __init__(self, lear):
+        super(Lear, self).__init__()
+        self.setShapeMode(QGraphicsPixmapItem.BoundingRectShape)
+        self.setFlag(QGraphicsItem.ItemSendsGeometryChanges)
+
+        if lear == core_const.LEAR_NOTHING:
+            self.setPixmap(QPixmap(f'{const.SUITS_DIR}/n.png'))
+            self.setToolTip('Козырь: нет')
+        else:
+            self.setPixmap(QPixmap(f'{const.SUITS_DIR}/{const.LEARS[lear]}.png'))
+            self.setToolTip(f'Козырь: {core_const.LEAR_NAMES[lear]}')
+
+
+class Area(QGraphicsRectItem):
+
+    def __init__(self, parent, size):
+        super(Area, self).__init__()
+        self.parent = parent
+        self.setRect(QRectF(0, 0, size[0], size[1]))
+        color = QColor(Qt.black)
+        color.setAlpha(50)
+        brush = QBrush(color)
+        self.setBrush(brush)
+        self.setPen(QPen(Qt.black))
+        self.setZValue(-1)
--- poker/gui/players_dlg.py
+++ poker/gui/players_dlg.py
@@ -3,21 +3,33 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
-from gui import const
-from modules.core import const as core_const
 from modules.core import helpers
+from gui import const
+from gui.graphics import Face2
 
 
 class PlayersDialog(QDialog):
 
     def __init__(self, parent, **kwargs):
         super().__init__(parent)
-        self._players = kwargs.get('players', [])
+        self._players_cnt = kwargs.get('players_cnt', 2)
+        self._cb_items = []
+        self._comboboxes = []
+
+        robots = sorted([r for r in const.ROBOTS])
+        for i, rob in enumerate(robots):
+            self._cb_items.append(helpers.Player())
+            self._cb_items[i].uid = i
+            self._cb_items[i].is_robot = True
+            self._cb_items[i].name = rob
+            self._cb_items[i].risk_level = random.randint(0, 2)
+
         self.setWindowIcon(QIcon(f'{const.RES_DIR}/player.ico'))
         self.setWindowTitle('Участники игры')
         self.setWindowFlags(self.windowFlags() & ~Qt.WindowContextHelpButtonHint)
         self.init_ui()
-        self.resize(800, 600)
+        # self.resize(800, 600)
+        # self.setFixedWidth(400)
 
     def init_ui(self):
         btn_ok = QPushButton("OK")
@@ -27,21 +39,60 @@ class PlayersDialog(QDialog):
         btn_cancel = QPushButton("Отмена")
         btn_cancel.setFixedWidth(140)
         btn_cancel.clicked.connect(self.reject)
-        buttonsBox = QHBoxLayout()
-        buttonsBox.addWidget(btn_ok)
-        buttonsBox.addWidget(btn_cancel)
+        buttons_box = QHBoxLayout()
+        buttons_box.addWidget(btn_ok)
+        buttons_box.addWidget(btn_cancel)
 
-        mainLayout = QVBoxLayout()
+        players_box = QHBoxLayout()
+        players_box.addWidget(QLabel('Количество участников парии (кроме вас)'))
+        sb = QSpinBox()
+        sb.setMinimum(2)
+        sb.setMaximum(3)
+        sb.setValue(self._players_cnt)
+        sb.valueChanged.connect(self._count_change)
+        players_box.addWidget(sb)
 
-        for p in self._players:
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(players_box)
+        selected = set()
+
+        for i in range(3):
             pb = QHBoxLayout()
             pb.setAlignment(Qt.AlignLeft)
+            lb = QLabel()
+            lb.setText(f'Игрок {i + 1}')
 
-            mainLayout.addLayout(pb)
+            cb = QComboBox()
+            cb.setEditable(False)
+            cb.setIconSize(QSize(64, 64))
+            cb.setFixedWidth(250)
+            cb.setEnabled(i < self._players_cnt)
 
-        mainLayout.addLayout(buttonsBox)
-        self.setLayout(mainLayout)
+            for el in self._cb_items:
+                cb.addItem(QIcon(Face2(el)), f'    {el.name}', QVariant(el))
 
-    @property
-    def players(self):
-        return self._players
+            i = random.randrange(0, len(self._cb_items))
+            while i in selected:
+                i = random.randrange(0, len(self._cb_items))
+            selected.add(i)
+            cb.setCurrentIndex(i)
+            self._comboboxes.append(cb)
+
+            pb.addWidget(lb)
+            pb.addWidget(cb)
+            main_layout.addLayout(pb)
+
+        main_layout.addLayout(buttons_box)
+        self.setLayout(main_layout)
+
+    def _count_change(self, value):
+        self._players_cnt = value
+        self._comboboxes[-1].setEnabled(len(self._comboboxes) <= self._players_cnt)
+
+    def get_players(self):
+        players = []
+
+        for i in range(self._players_cnt):
+            players.append(self._comboboxes[i].currentData())
+
+        return players
