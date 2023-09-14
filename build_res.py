e383d66e93a97d8b80bcfc9272bbbabfd787821e
Балычков Вячеслав <balychkov@bobday.ru>
2023-09-14 20:36:09 +0300

    Сделал фишку с показом баек в игре

--- build_res.py
+++ build_res.py
@@ -1,9 +1,15 @@
 import os
 import json
 import random
+import argparse
 
 
-def reload_res():
+def reload_res(source_dir: str):
+    if not source_dir:
+        print('Не указана папка с исходниками!')
+        return
+
+    print(f'Source folder {source_dir}')
     s = input('Вы сейчас собираетесь перетереть существующий ресурс тем, что найдется в исходниках.'
               ' Вы точно уверены, что это надо сделать? [ДА: Y/y/Д/д, НЕТ: N/n/Н/н]')
 
@@ -14,14 +20,17 @@ def reload_res():
     print('Building resources')
 
     data = []
-    rootdir = './resources/anecdots'
+    cnt = 0
 
-    for fn in os.listdir(rootdir):
-        with open(f'{rootdir}/{fn}', 'r') as f:
+    for fn in os.listdir(source_dir):
+        cnt += 1
+        with open(f'{source_dir}/{fn}', 'r') as f:
             contents = f.read()
             contents = contents.split('\n\n')
             data.extend(contents)
 
+    print(f'Found source files: {cnt}')
+    print(f'Fond total texts: {len(data)}')
     print('Writing res file')
 
     with open('./resources/bikes.json', 'wb') as f:
@@ -34,12 +43,31 @@ def test():
     with open('./resources/bikes.json', 'rb') as f:
         data = json.loads(f.read().decode())
         n = 10 if len(data) >= 10 else len(data)
-        print(f'Вот {n} случано выбранных анекдотов из того, что есть сейчас:\n\n')
+        print(f'Вот {n} случано выбранных текстов из того, что есть сейчас:\n\n')
 
         for r in range(n):
             print(random.choice(data))
             print('\n *** \n')
 
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--build', '-b', action='store_true',
+                    help='Пересобрать файл текстовых ресурсов из исходников. Существующий файл будет перезаписан новым.'
+                         'Исходники - все текстовые файлы в указанной папке, содержащие тексты (афоризмы, анектоды), '
+                         'разделенные пустой строкой.')
+    ap.add_argument('--source', '-s', help='Папка с исходными текстами. Все текстовые файлы (*.txt) '
+                                           'в этой папке будут обработаны и собраны в ресурс текстов.')
+    ap.add_argument('--test', '-t', action='store_true',
+                    help='Протестировать результат или текущий собранный ресурс - выведет случайные 10 текстов')
+    args = ap.parse_args()
+
+    if args.build:
+        reload_res(args.source)
+
+    if args.test:
+        test()
+
+
 if __name__ == '__main__':
-    reload_res()
-    test()
+    main()
