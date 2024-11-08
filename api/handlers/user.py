8ed620c2189603c8b579ac09ccdde58c9cb1aa88
vyacheslav99 <warlock999@mail.ru>
2024-11-08 10:23:22 +0300

    #30 Ручки изменения и удаления аватарки

--- api/handlers/user.py
+++ api/handlers/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile
 
 from api.models.security import UserPublic
 from api.models.user import UserPatchBody
@@ -11,3 +11,13 @@ router = APIRouter(prefix='/api', tags=['user'])
 @router.patch('/user', response_model=UserPublic)
 async def change_user(body: UserPatchBody, curr_user: CheckAuthProvider):
     return await UserService().change_user(curr_user, body)
+
+
+@router.put('/user/avatar', response_model=UserPublic)
+async def change_avatar(file: UploadFile, curr_user: CheckAuthProvider):
+    return await UserService().change_avatar(curr_user, file)
+
+
+@router.delete('/user/avatar', response_model=UserPublic)
+async def clear_avatar(curr_user: CheckAuthProvider):
+    return await UserService().clear_avatar(curr_user)
