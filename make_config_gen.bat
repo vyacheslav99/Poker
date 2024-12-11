a16d27600cb7414ee8fb4a74e9f10fa17c11c175
vyacheslav99 <warlock999@mail.ru>
2024-12-11 13:05:43 +0300

    #50 Сборщик exe

--- make_config_gen.bat
+++ make_config_gen.bat
@@ -1,1 +1,3 @@
+set "TCL_LIBRARY=C:\Users\slava\AppData\Local\Programs\Python\Python313\tcl\tcl8.6\"
+set "TK_LIBRARY=C:\Users\slava\AppData\Local\Programs\Python\Python313\tcl\tk8.6\"
 pipenv run python -m auto_py_to_exe
